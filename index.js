3e76bc2ffa9413085a4a84571ff293c03103956b
Christopher Arias <chrstphr.aaron@gmail.com>
2020-07-22 01:44:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function decrement(n) {
   return n -= 1;
 }
 
-function makeInt(n){
+function makeInt(n) {
   return parseInt(n, 10);
 }
 
-function preserveDecimal(n){
+function preserveDecimal(n) {
   return parseFloat(n);
 }
